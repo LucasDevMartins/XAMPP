a41bd1e4e3e0fac40ef5150d91aec44c042457eb
Lucas Martins <lucas.rodriguesmartins@hotmail.com>
2024-07-01 07:44:17 -0300

    comando echo e print

---
+++ echo_print.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>CURSO PHP</title>
+	</head>
+
+	<body>
+
+		<?php
+
+			//a ultima instrução ; não é necessario, mas é bom colocar
+			echo 'Comando echo<br/>';
+
+			echo "Lucas Martins<hr/>";
+
+			echo print("Comando print<hr/>");
+
+		?>
+
+	</body>
+</html>
--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-teste
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>CURSO PHP</title>
+	</head>
+
+	<body>
+
+		<?php
+		//o ; é obrigado
+			echo 'Utilizando a tag padrão';
+		?>
+
+		<br><br>
+
+		<?= 'Utilizando a tag impressão'?>
+
+	</body>
+</html>
